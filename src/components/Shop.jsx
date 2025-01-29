dd52aa0e98bb47155bdc33a124a4e3e988a7038c
ShrikeSwallow <maciej.jaskolka@gmail.com>
2025-01-29 05:45:00 +0000

    Add fetching and displaying data from an external API

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import Navigation from "./Navigation";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+
+    const fetchFakeStore = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      if (!ignore) {
+        setProducts(data);
+      }
+    };
+    fetchFakeStore();
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <>
       <Navigation />
       <h1>Available products</h1>
+      {products.map((product) => (
+        <p key={product.id}>{product.title}</p>
+      ))}
     </>
   );
 };
