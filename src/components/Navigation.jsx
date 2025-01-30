9af4bfc56662e76daef61ea50d0618a8160ef187
ShrikeSwallow <maciej.jaskolka@gmail.com>
2025-01-30 06:48:30 +0000

    Style changes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,15 +3,21 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <>
-      <nav>
-        <ul>
+      <nav className="mb-8 mt-3">
+        <ul className="flex">
           <li>
-            <NavLink to="/" className="aria-[current=page]:bg-red-700">
+            <NavLink
+              to="/"
+              className="px-8 py-4 aria-[current=page]:bg-red-600"
+            >
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/shop" className="aria-[current=page]:bg-red-700">
+            <NavLink
+              to="/shop"
+              className="bg-slate-600 px-8 py-4 aria-[current=page]:bg-red-600"
+            >
               Shop
             </NavLink>
           </li>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -23,16 +23,40 @@ const Shop = () => {
   return (
     <>
       <Navigation />
-      <h1>Available products</h1>
-      <div className="products-container grid grid-cols-6 gap-12">
-        {products.map((product) => (
-          <div key={product.id} className="product-card flex flex-col">
-            <img src={product.image}></img>
-            <p className="product-name">{product.title}</p>
-            <p className="product-price">${product.price}</p>
-          </div>
-        ))}
-      </div>
+      <main className="mb-8 ml-[6.5rem] mr-[6.5rem]">
+        <h1 className="mb-6 text-4xl font-bold">Available products</h1>
+        <div className="products-container grid grid-cols-2 gap-6 lg:grid-cols-3 lg:gap-9 xl:grid-cols-4 xl:gap-12">
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="product-card flex flex-col gap-1 rounded-lg bg-slate-50 p-4 text-gray-900 shadow-md shadow-slate-900"
+            >
+              <img
+                src={product.image}
+                className="mb-2 h-3/4 object-contain"
+              ></img>
+              <p className="product-name flex-1 font-semibold">
+                {product.title}
+              </p>
+              <p className="product-price">${product.price}</p>
+              <label htmlFor="quantity" className="self-center text-sm">
+                Quantity{" "}
+              </label>
+              <div className="grid grid-cols-3">
+                <button className="">-</button>
+                <input
+                  className="bg-slate-50 text-center text-sm"
+                  id="quantiy"
+                  name="quantity"
+                  type="text"
+                  value={0}
+                />
+                <button className="">+</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </>
   );
 };
